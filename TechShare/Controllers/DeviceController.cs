3051d436076c181e98133895f399bc4b236a0f04
BondSensei <phuphamwork89@gmail.com>
2026-06-10 08:51:24 +0700

    Thêm trang Upload (Đăng) thiết bị cho thuê

--- TechShare/Controllers/DeviceController.cs
+++ TechShare/Controllers/DeviceController.cs
@@ -2,18 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TechShare.Data;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechShare.ViewModels;
+using System.Security.Claims;
+using TechShare.Models;
 
 namespace TechShare.Controllers
 {
     public class DeviceController : Controller
     {
         private readonly TechShareDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public DeviceController(TechShareDbContext context)
+        public DeviceController(TechShareDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
+        // Trang chi tiết của thiết bị
         public async Task<IActionResult> Detail(int id)
         {
             var device = await _context.Devices
@@ -31,5 +39,65 @@ namespace TechShare.Controllers
 
             return View(device);
         }
+
+        // Trang upload thiết bị
+        [Authorize]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+        // Xử lý đăng thiết bị cho thuê lên
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DeviceCreateViewModel model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = "";
+
+                // Lưu file ảnh
+                if (model.ImageFile != null)
+                {
+                    string uploadsFolder = Path.Combine(_env.WebRootPath, "images");
+
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageFile.FileName;
+
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.ImageFile.CopyToAsync(fileStream);
+                    }
+                }
+
+                // Tạo thông tin thiết bị lưu vào DB
+                var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var device = new Device()
+                {
+                    Name = model.Name,
+                    CategoryId = model.CategoryId,
+                    PricePerDay = model.PricePerDay,
+                    DepositAmount = model.DepositAmount,
+                    StockQuantity = model.StockQuantity,
+                    ImageUrl = "/images" + uniqueFileName,
+                    OwnerId = int.Parse(userIdStr)
+                };
+
+                _context.Devices.Add(device);
+                await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "Đăng bài thành công!";
+                return RedirectToAction("Index", "Profile");
+            }
+
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+            return View(model);
+
+        }
+
     }
 }
---
+++ TechShare/ViewModels/DeviceCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechShare.ViewModels
+{
+    public class DeviceCreateViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên thiết bị")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng chọn danh mục")]
+        public int CategoryId { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập giá thuê/ngày")]
+        public decimal PricePerDay { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tiền cọc")]
+        public decimal DepositAmount { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số lượng tồn kho")]
+        public int StockQuantity { get; set; }
+
+
+        [Required(ErrorMessage = "Vui lòng chọn ảnh thiết bị")]
+        public IFormFile ImageFile { get; set; } = null!;
+    }
+}
