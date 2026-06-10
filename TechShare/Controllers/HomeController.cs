56d0ffd227332d1c25a573b2115350bbb98f276d
BondSensei <phuphamwork89@gmail.com>
2026-06-10 10:31:09 +0700

    Cập nhật lại chức năng lọc cho project

--- TechShare/Controllers/HomeController.cs
+++ TechShare/Controllers/HomeController.cs
@@ -17,29 +17,23 @@ namespace TechShare.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string? searchQuery, int? categoryId)
+        public async Task<IActionResult> Index(string q)
         {
             var query = _context.Devices
                 .Include(d => d.Owner)    
                 .Include(d => d.Category) 
                 .Where(d => d.Status == DeviceStatus.Available && d.StockQuantity > 0);
 
-            if (!string.IsNullOrEmpty(searchQuery))
+            if (!string.IsNullOrEmpty(q))
             {
-                query = query.Where(d => d.Name.Contains(searchQuery));
-            }
-
-            if (categoryId.HasValue)
-            {
-                query = query.Where(d => d.CategoryId == categoryId.Value);
+                query = query.Where(d => d.Name.Contains(q));
             }
 
             var devices = await query
                 .OrderByDescending(d => d.Id) 
                 .ToListAsync();
 
-            ViewBag.Categories = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(await _context.Categories.ToListAsync(), "Id", "Name", categoryId);
-            ViewBag.SearchQuery = searchQuery;
+            ViewData["SearchQuery"] = q;
 
             return View(devices);
         }
