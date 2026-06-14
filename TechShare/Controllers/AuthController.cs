29c5af857c463ba3f17fcbf1d8a0f0908bd2baec
BondSensei <phuphamwork89@gmail.com>
2026-06-14 09:55:39 +0700

    Cập nhật lại role Admin khi đăng nhập, fix UI sticky cho footer

--- TechShare/Controllers/AuthController.cs
+++ TechShare/Controllers/AuthController.cs
@@ -38,7 +38,8 @@ namespace TechShare.Controllers
                     {
                         new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                         new Claim(ClaimTypes.Name, user.FullName),
-                        new Claim(ClaimTypes.Email, user.Email)
+                        new Claim(ClaimTypes.Email, user.Email),
+                        new Claim(ClaimTypes.Role, user.Role)
                     };
 
                     var identity = new ClaimsIdentity(claims, "Cookies");
